4f74b7b3b06fe79ea6fb6631c1faba0a2b8b141c
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-12-04 10:13:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Alexander Williams
+# 12/4/2024
+# Web and App Development
+
+
+
+
+
+# Simple Increment and Decrement
+
+
+my_score = 85
+your_score = 90
+
+my_score += 10
+
+your_score -= 20
+
+print(my_score)
+print(your_score)
+
+
+# Counting up with a while loop
+
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
+
+# Counting down with a while loop
+    
+countdown = 10
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+print('We have liftoff!')
